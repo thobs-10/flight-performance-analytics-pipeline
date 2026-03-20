71c1edfeef0ad090ae18a36ef7d1d8941b86f059
thobela <thobela.sixpence@gmail.com>
2026-03-20 22:18:21 +0200

    definition script to combine together assets

--- src/flight_performance_analytics_pipeline/definitions.py
+++ src/flight_performance_analytics_pipeline/definitions.py
@@ -1,3 +1,25 @@
-from dagster import Definitions
+from dagster import Definitions, EnvVar
 
-defs = Definitions()
+from flight_performance_analytics_pipeline.assets.bronze.load_to_postgres import (
+    add_metadata_columns_to_airline_delay_data,
+    read_raw_airline_delay_csv,
+    write_to_bronze_airline_delay_data,
+)
+from flight_performance_analytics_pipeline.resources.postgres_resource import PostgresResource
+
+defs = Definitions(
+    assets=[
+        read_raw_airline_delay_csv,
+        add_metadata_columns_to_airline_delay_data,
+        write_to_bronze_airline_delay_data,
+    ],
+    resources={
+        "postgres": PostgresResource(
+            host=EnvVar("POSTGRES_HOST"),
+            port=EnvVar.int("POSTGRES_PORT"),
+            user=EnvVar("POSTGRES_USER"),
+            password=EnvVar("POSTGRES_PASSWORD"),
+            database=EnvVar("POSTGRES_DB"),
+        )
+    },
+)
